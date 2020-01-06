2259069b136073ed2e2548c2263a8bd318647fff
Enas-Samir <inass.helmy@gmail.com>
2020-01-06 23:16:06 +0200

    add file contains all fontawesome classes

---
+++ src/components/player/controls.js
@@ -0,0 +1,7 @@
+export const play = 'fa fa-play fa-lg',
+  pause = 'fa fa-pause fa-lg',
+  next = 'fa fa-step-forward fa-lg',
+  previous = 'fa fa-step-backward fa-lg',
+  shuffle = 'fa fa-random fa-lg',
+  volume = 'fa fa-volume-up fa-lg',
+  mute = 'fa fa-volume-off fa-lg';
