c64247571081209cdce02c93e17300d1ede0420b
Enas Samir <inass.helmy@gmail.com>
2020-01-07 08:09:41 +0200

    Updates

---
+++ precache-manifest.1adf6ff6d84e2dba27fe77d95ce30da8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8264f99172a504d8164dfcf21a1dac3",
+    "url": "/Music-Player/index.html"
+  },
+  {
+    "revision": "ea632fd8832606547305",
+    "url": "/Music-Player/static/css/2.3bd2016b.chunk.css"
+  },
+  {
+    "revision": "81f005322e081b8a1ca3",
+    "url": "/Music-Player/static/css/main.0fed1613.chunk.css"
+  },
+  {
+    "revision": "ea632fd8832606547305",
+    "url": "/Music-Player/static/js/2.27513468.chunk.js"
+  },
+  {
+    "revision": "a97f0bc829421db34c7d17105b29183b",
+    "url": "/Music-Player/static/js/2.27513468.chunk.js.LICENSE"
+  },
+  {
+    "revision": "81f005322e081b8a1ca3",
+    "url": "/Music-Player/static/js/main.3782aaf9.chunk.js"
+  },
+  {
+    "revision": "d04ac8eca35854eed878",
+    "url": "/Music-Player/static/js/runtime-main.a4626258.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Music-Player/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Music-Player/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Music-Player/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Music-Player/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Music-Player/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
