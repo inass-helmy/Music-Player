30c644c4bbabac1c963f8d664fd56ba84203b26d
Enas Samir <inass.helmy@gmail.com>
2020-01-21 16:57:44 +0200

    add features to the player: shuffle, repeat,volume

--- src/components/player/player.js
+++ src/components/player/player.js
@@ -1,58 +1,120 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './player.css';
-import { play, pause, next, previous } from './controls';
+import { play, pause, next, previous, shuffle, volume, mute, repeat } from './controls';
 
 const Player = props => {
   const {
     isPlaying,
-    playFirstSong,
-    firstSong,
-    pauseSong,
     togglePlaying,
-    resumeSong,
-    isClicked,
     seek,
     moveBar,
     duration,
     playNext,
-    playPrevious
+    playPrevious,
+    shuffleList,
+    isShuffled,
+    changeVol,
+    repeatSong,
+    isRepeat,
+    songVolume
   } = props;
 
   const playPauseSwitch = isPlaying ? pause : play;
   const selector = React.createRef();
+  const [volumeLevel, setVolume] = useState(songVolume || 0.5);
+  const [isMute, setMute] = useState(false);
+  const [volClass, setClass] = useState(volume);
+  const [color, setColor] = useState('');
 
-  const switchPlayPause = () => {
-    togglePlaying();
-    isPlaying ? pauseSong() : isClicked ? resumeSong() : playFirstSong(firstSong.url, firstSong.id);
+  const timePrettier = time => {
+    let min = [0, Math.floor(time / 60)];
+    let sec = [0, Math.floor(time - min[1] * 60)];
+    let dMin = min[1] < 10 ? min.join('') : min[1];
+    let dSec = sec[1] < 10 ? sec.join('') : sec[1];
+    return dMin + ':' + dSec;
   };
+
+  const displayDuration = timePrettier(duration);
+  const displaySeek = timePrettier(parseInt(seek));
 
   return (
     <div className="player-controls">
-      <button className="player-btn" onClick={() => playPrevious()}>
+      <button
+        className={`player-btn ${isShuffled ? 'fill-color' : 'controls'}`}
+        onClick={() => shuffleList()}
+      >
+        <i className={shuffle} aria-hidden="true"></i>
+      </button>
+      <button className="player-btn controls" onClick={() => playPrevious()}>
         <i className={previous} aria-hidden="true"></i>
       </button>
       <button
-        className="player-btn"
+        className="player-btn play"
         onClick={() => {
-          switchPlayPause();
+          togglePlaying();
         }}
       >
         <i className={playPauseSwitch} aria-hidden="true"></i>
       </button>
-      <button className="player-btn" onClick={() => playNext()}>
+      <button className="player-btn controls" onClick={() => playNext()}>
         <span className={next} aria-hidden="true"></span>
       </button>
-      <progress
-        ref={selector}
-        className="slider"
-        value={seek}
-        max={duration}
-        onClick={e => {
-          var rect = selector.current.getBoundingClientRect();
-          var percentage = (e.clientX - rect.left) / selector.current.offsetWidth;
-          moveBar(percentage * 100);
+      <div className="slider">
+        <span className="player-btn seek">{displaySeek}</span>
+        <input
+          className="progress-bar"
+          type="range"
+          name="progressBar"
+          ref={selector}
+          value={seek}
+          max={duration}
+          onChange={e => {
+            moveBar(parseInt(e.target.value) || seek);
+          }}
+        ></input>
+        <span className="song-duration player-btn">{displayDuration}</span>
+      </div>
+      <button
+        className={`player-btn ${isRepeat ? 'fill-color' : 'controls'}`}
+        onClick={() => repeatSong()}
+      >
+        <i className={repeat}></i>
+      </button>
+
+      <button
+        className="player-btn volume-btn controls"
+        onClick={() => {
+          setMute(isMute => !isMute);
+          isMute ? setClass(volume) : setClass(mute);
+          setVolume(+isMute / 2);
+          changeVol(+isMute / 2);
         }}
-      ></progress>
+      >
+        <i className={volClass} aria-hidden="true"></i>
+      </button>
+      <input
+        className={`volume-range ${color}`}
+        type="range"
+        name="volume"
+        value={volumeLevel}
+        min="0"
+        max="1"
+        step="0.1"
+        onChange={e => {
+          if (e.target.value <= 0) {
+            setClass(mute);
+            setMute(true);
+            setVolume(0);
+            changeVol(0);
+          } else {
+            setMute(false);
+            setVolume(e.target.value);
+            changeVol(e.target.value);
+            setClass(volume);
+            setColor('color');
+          }
+        }}
+      ></input>
     </div>
   );
 };
