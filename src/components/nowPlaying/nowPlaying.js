6a99667072d24ce5460bce0eafea522dc044714b
Enas Samir <inass.helmy@gmail.com>
2020-01-21 16:55:53 +0200

    change colors of the nowplaying view box

--- src/components/nowPlaying/nowPlaying.js
+++ src/components/nowPlaying/nowPlaying.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './nowPlaying.css';
 
-const NowPlaying = ({ color, description, userName, nowPlayingSong, artist }) => {
+const NowPlaying = ({ nowPlayingSong }) => {
   return (
-    <div className="nowplaying-song" style={{ backgroundColor: color }}>
+    <div
+      className="nowplaying-song"
+      style={{
+        backgroundImage: `linear-gradient(26deg, #efefe1 0%, ${nowPlayingSong.color}  80%)`
+      }}
+    >
       <h2 className="song-name">
         {nowPlayingSong.songName}
         <br />
-        <span className="artist">{artist}</span>
+        <span className="artist">{nowPlayingSong.artist}</span>
       </h2>
 
       <p className="song-info">{nowPlayingSong.description}</p>
