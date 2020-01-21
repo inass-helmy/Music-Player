f94091986ce84c63b33e35c4e0db35054528fb26
Enas Samir <inass.helmy@gmail.com>
2020-01-21 17:00:01 +0200

    add new features: rep,shuffle,col control

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import Playlist from '../components/playlist/playlist';
 import Player from '../components/player/player';
 import NowPlaying from '../components/nowPlaying/nowPlaying';
 import { Howl } from 'howler';
+import { colors } from '../assets/colors';
 
 let song = null;
 class App extends Component {
@@ -11,12 +12,17 @@ class App extends Component {
     super(props);
     this.state = {
       isLoaded: false,
-      isPlaying: false,
+      isPlaying: song != null ? song.playing() : false,
       isClicked: false,
-      shuffled: false,
+      isRepeat: false,
+      isShuffled: false,
+      headerClass: 'transparent',
       seek: 0,
       duration: 0,
       currentIndex: 0,
+      shuffleIndex: 0,
+      isPaused: false,
+      volume: song === null ? 0.5 : song.volume(),
       song: { id: 0, songName: '', url: '', description: '', artist: '' },
       data: [],
       displayList: [],
@@ -34,18 +40,33 @@ class App extends Component {
       .then(res => res.json())
       .then(json => {
         console.log(json);
-        const data = json.map(song => {
+        const songsColors = colors.splice(0, json.length);
+
+        const data = json.map((song, index) => {
           return {
             songName: song.upload_name,
             id: song.upload_id,
             url: song.files[0].download_url,
             description: song.upload_description_plain,
-            artist: song.user_name
+            artist: song.user_name,
+            color: songsColors[index]
           };
         });
         this.setState({ isLoaded: true, data });
       });
+
+    window.addEventListener('scroll', this.listenToScroll);
   }
+
+  listenToScroll = e => {
+    if (window.scrollY < 50) {
+      this.setState({ headerClass: 'transparent' });
+    } else if (window.scrollY > 100 && window.scrollY < 150) {
+      this.setState({ headerClass: 'in-between' });
+    } else {
+      this.setState({ headerClass: 'obaque' });
+    }
+  };
 
   playSong = (src, id) => {
     //check if song is null, if not stop previous song and unload it
@@ -54,10 +75,9 @@ class App extends Component {
       song.unload();
       song = null;
     }
-    const { data } = this.state;
+    const { data, isRepeat, volume } = this.state;
     const temp = data.filter(song => song.id !== id);
     const currentIndex = data.findIndex(song => song.id === id);
-    console.log('index', currentIndex);
     this.setState({
       currentIndex,
       currentSrc: src,
@@ -68,17 +88,19 @@ class App extends Component {
     song = new Howl({
       src,
       html5: true,
+      volume: volume,
+      loop: isRepeat,
+      preload: true,
       onload: () => {
-        this.setState({ duration: song.duration() });
+        this.setState({ duration: Math.floor(song.duration()), seek: 0 });
       },
       onplay: function() {
-        this.setState({ seek: song.seek() });
         setInterval(() => {
-          this.setState({ seek: Math.round(song.seek() || 0) });
+          this.setState({ seek: parseInt(song.seek()) });
         }, 1000);
       }.bind(this),
       onend: function() {
-        this.playNextSong();
+        isRepeat ? song.play() : this.playNextSong();
       }.bind(this)
     });
     song.play();
@@ -86,55 +108,91 @@ class App extends Component {
 
   playNextSong = () => {
     this.setState({ seek: 0 });
-    const { currentIndex, data } = this.state;
-    const nextSong = currentIndex === data.length - 1 ? data[0] : data[currentIndex + 1];
-    this.playSong(nextSong.url, nextSong.id);
+    const { currentIndex, data, isShuffled, shuffledList, shuffleIndex, isRepeat } = this.state;
+    let nextSong;
+    if (isRepeat) {
+      nextSong = data[currentIndex];
+      this.playSong(nextSong.url, nextSong.id);
+      return;
+    } else if (isShuffled) {
+      nextSong = data[shuffledList[shuffleIndex]];
+      this.setState({
+        shuffleIndex: this.state.shuffleIndex === data.length - 1 ? 0 : this.state.shuffleIndex + 1
+      });
+      this.playSong(nextSong.url, nextSong.id);
+    } else {
+      nextSong = currentIndex === data.length - 1 ? data[0] : data[currentIndex + 1];
+      this.playSong(nextSong.url, nextSong.id);
+    }
   };
 
   playPreviousSong = () => {
     this.setState({ seek: 0 });
-    const { currentIndex, data } = this.state;
-    const previousSong = currentIndex === 0 ? data[data.length - 1] : data[currentIndex - 1];
-    this.playSong(previousSong.url, previousSong.id);
-  };
 
-  pauseSong = () => {
-    if (this.state.isPlaying) {
-      song.pause();
+    const { currentIndex, data, isShuffled, shuffledList, shuffleIndex, isRepeat } = this.state;
+    let prevSong;
+    if (isRepeat) {
+      prevSong = data[currentIndex];
+      this.playSong(prevSong.url, prevSong.id);
+    } else if (isShuffled) {
+      prevSong = data[shuffledList[shuffleIndex]];
+      this.setState({
+        shuffleIndex: this.state.shuffleIndex === 0 ? data.length - 1 : this.state.shuffleIndex - 1
+      });
+      this.playSong(prevSong.url, prevSong.id);
+    } else {
+      prevSong = currentIndex === 0 ? data[data.length - 1] : data[currentIndex - 1];
+      this.playSong(prevSong.url, prevSong.id);
     }
   };
 
   togglePlaying = () => {
+    const { data, isClicked } = this.state;
+    this.state.isPlaying
+      ? song.pause()
+      : isClicked
+      ? song.play()
+      : this.playSong(data[0].url, data[0].id);
+
     this.setState({
       isPlaying: !this.state.isPlaying
     });
   };
 
-  resumeSong = () => {
-    song.play();
-  };
-
   moveBar = position => {
-    const { duration } = this.state;
     this.setState({
-      seek: (position * duration) / 100
+      seek: parseInt(position)
     });
-    song.seek((position * duration) / 100);
+    song.seek(parseInt(position));
   };
 
+  shuffleList = () => {
+    const { data, currentIndex } = this.state;
+    let i = data.length;
+    let list = [];
+    while (--i > 0) {
+      let index = Math.round(Math.random() * (i + 1));
+      list.push(index);
+    }
+    this.setState({
+      shuffledList: list,
+      isShuffled: !this.state.isShuffled,
+      shuffleIndex: currentIndex
+    });
+  };
+  setVolume = vol => {
+    if (song != null) {
+      vol === 0 ? song.mute(true) : song.mute(false);
+      song.volume(vol);
+    } else {
+      this.setState({ volume: vol });
+    }
+  };
+
+  repeatSong = () => {
+    this.setState({ isRepeat: !this.state.isRepeat });
+  };
   render() {
-    const colors = [
-      '#581845',
-      '#900c3f',
-      '#c70039',
-      '#c0392b',
-      '#900c3f',
-      '#58355e',
-      '#cc0000',
-      '#f4511e',
-      '#c0392b',
-      '#c70039'
-    ];
     const {
       data,
       isClicked,
@@ -143,17 +201,30 @@ class App extends Component {
       isPlaying,
       seek,
       duration,
-      currentIndex
+      currentIndex,
+      isShuffled,
+      headerClass,
+      isRepeat,
+      volume
     } = this.state;
     const display = isClicked && isLoaded ? displayList : data;
+    const justify = isClicked ? 'title-tracks-shift' : 'title-tracks-auto';
 
     return (
       this.state.isLoaded && (
         <div className="container">
-          <div className="audio-tracks">
-            <div className="playlist">
-              <h4 className="title">AVAILABLE SOUNDTRACKS</h4>
-              {display.map((song, index) => {
+          <header className={headerClass}>
+            {(isClicked || isPlaying) && <h4 className="title title-nowplaying">NOW PLAYING</h4>}
+            <h4 className={`title ${justify}`}>AVAILABLE SOUNDTRACKS</h4>
+          </header>
+          <div className="playlist">
+            {(isClicked || isPlaying) && (
+              <div className="now-playing clicked">
+                <NowPlaying className="now-playing " nowPlayingSong={data[currentIndex]} />
+              </div>
+            )}
+            <div className="audio-tracks">
+              {display.map(song => {
                 return (
                   <Playlist
                     key={song.id}
@@ -162,33 +233,31 @@ class App extends Component {
                     id={song.id}
                     artist={song.artist}
                     playSong={this.playSong}
-                    color={colors[index]}
+                    color={song.color}
+                    isClicked={isClicked}
                   />
                 );
               })}
             </div>
-            {(isClicked || isPlaying) && (
-              <div className="now-playing">
-                <h4 className="title">NOW PLAYING</h4>
-                {<NowPlaying nowPlayingSong={data[currentIndex]} color={colors[currentIndex]} />}
-              </div>
-            )}
           </div>
-          <Player
-            firstSong={data[0]}
-            playFirstSong={this.playSong}
-            resumeSong={this.resumeSong}
-            pauseSong={this.pauseSong}
-            isPlaying={isPlaying}
-            isClicked={isClicked}
-            togglePlaying={this.togglePlaying}
-            seek={seek}
-            duration={duration}
-            moveBar={this.moveBar}
-            songDuration={Math.round(duration)}
-            playNext={this.playNextSong}
-            playPrevious={this.playPreviousSong}
-          />
+          <footer>
+            <Player
+              isPlaying={isPlaying}
+              togglePlaying={this.togglePlaying}
+              seek={seek}
+              duration={duration}
+              moveBar={this.moveBar}
+              songDuration={duration}
+              playNext={this.playNextSong}
+              playPrevious={this.playPreviousSong}
+              shuffleList={this.shuffleList}
+              isShuffled={isShuffled}
+              changeVol={this.setVolume}
+              repeatSong={this.repeatSong}
+              isRepeat={isRepeat}
+              songVolume={volume}
+            />
+          </footer>
         </div>
       )
     );
